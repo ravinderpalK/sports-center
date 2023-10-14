ace43024a3c51957119b85ac16837504296380be
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-14 16:42:17 +0530

    updated HomePageContainer

--- src/layouts/home/HomePageContainer.tsx
+++ src/layouts/home/HomePageContainer.tsx
@@ -1,9 +1,22 @@
 import React from "react"
 import LiveGames from "../../views/live_games";
+import Articles from "../../views/articles";
+import Filter from "../../views/filter";
 
 const HomePageContainer: React.FC = () => {
   return (
-    <LiveGames />
+    <div className="mx-6">
+      <LiveGames />
+      <h2>Trending News</h2>
+      <div className="flex">
+        <div className="w-4/5 border-2">
+          <Articles />
+        </div>
+        <div className="border-2 w-1/5">
+          <Filter />
+        </div>
+      </div>
+    </div>
   )
 };
 
