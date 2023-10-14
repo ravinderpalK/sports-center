ba9776ee9652638311fa5791b698767dafb7110d
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-14 16:39:31 +0530

    updated home router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomeLayout from "../layouts/home";
-import { MatchesProvider } from "../context/matches/context";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <MatchesProvider>
-        <HomeLayout />
-      </MatchesProvider>
+      <HomeLayout />
     )
   },
 ])
