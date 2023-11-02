b55f42a44dd3172436ecb51de9a9dc574de3ef73
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-02 12:27:20 +0530

    updated layout for logged in user

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Appbar from "./Appbar";
 import HomePageContainer from "./HomePageContainer";
+import { usePreferencesDispatch } from "../../context/user_preferences/context";
+import { fetchPreferences } from "../../context/user_preferences/actions";
 
 
 const HomeLayout: React.FC = () => {
+  const isAuthenticated = !!localStorage.getItem("authToken");
+  const prefrencesDispatch = usePreferencesDispatch();
+  useEffect(() => {
+    console.log(isAuthenticated)
+    if (isAuthenticated) {
+      fetchPreferences(prefrencesDispatch);
+    }
+  }, [prefrencesDispatch]);
+
   return (
     <div className="h-screen">
       <div className="">
