d03d6693f409e78322c0a83f1eac006ce98efaa6
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-05 17:13:30 +0530

    updated style for small device

--- src/views/live_games/index.tsx
+++ src/views/live_games/index.tsx
@@ -4,7 +4,7 @@ import LiveGamesContainer from "./LiveGamesContainer";
 const LiveGames: React.FC = () => {
   return (
     <div >
-      <h2 className="text-xl font-bold mt-2">Live Games</h2>
+      <h2 className="text-lg lg:text-xl font-bold mt-2">Live Games</h2>
       <LiveGamesContainer />
     </div >
   )
