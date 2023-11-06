b10000ebc595329c043caeb72b740b8b51471178
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-06 16:50:15 +0530

    updated sign and signup page

--- src/views/signin/index.tsx
+++ src/views/signin/index.tsx
@@ -4,13 +4,13 @@ import SigninForm from "./SigninForm";
 
 const Signin: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Sign in</h1>
+    <div className="h-full flex flex-col items-center justify-center bg-gray-50 text-xs lg:text-base">
+      <div className="max-w-lg w-full px-6 py-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-xl lg:text-3xl font-bold text-center text-gray-800 mb-8">Sign in</h1>
         <SigninForm />
       </div>
       <div>
-        <div className="mt-4 text-base">
+        <div className="mt-4 lg:text-base">
           New to Sports-center?
           <Link to="/signup" className="px-2">Signup</Link>
         </div>
