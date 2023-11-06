0bd83e30e5dbafc18419185cf985bce9cb6ba220
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-06 16:51:18 +0530

    updated signin and signup view

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,26 +3,31 @@ import HomeLayout from "../layouts/home";
 import Signin from "../views/signin";
 import Signup from "../views/signup";
 import Signout from "../views/singnout";
+import HomePage from "../views/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <HomeLayout />
-    )
+    ),
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "signin",
+        element: <Signin />
+      },
+      {
+        path: "signup",
+        element: <Signup />
+      },
+      {
+        path: "signout",
+        element: <Signout />
+      },
+    ]
   },
-  {
-    path: "/signin",
-    element: <Signin />
-  },
-  {
-    path: "/signup",
-    element: <Signup />
-  },
-  {
-    path: "/signout",
-    element: <Signout />
-  },
+
 ])
 
 export default router;
