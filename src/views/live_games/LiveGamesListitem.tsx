08afac4cab46ea591db88873bad2a7861e2f6ad7
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-08 00:22:22 +0530

    added aria label for button

--- src/views/live_games/LiveGamesListitem.tsx
+++ src/views/live_games/LiveGamesListitem.tsx
@@ -104,7 +104,7 @@ const LiveGamesListItem = (props: any) => {
         <span className="font-semibold">{match.teams[1].name}</span>
         <span className="float-right">{match.score[match.teams[1].name]}</span>
       </div>
-      <button onClick={() => fetchMatchDetails(dispatch, match.id)} className="absolute top-2 right-2">
+      <button onClick={() => fetchMatchDetails(dispatch, match.id)} aria-label="RefreshGame" className="absolute top-2 right-2">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
         </svg>
