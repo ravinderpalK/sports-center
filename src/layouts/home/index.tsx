41ec2dd493cd5e528478e3f0f63f545b465d0ec5
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-07 12:58:45 +0530

    removed fetch prefrerences method

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,20 +1,10 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import Appbar from "./Appbar";
-import { usePreferencesDispatch } from "../../context/user_preferences/context";
-import { fetchPreferences } from "../../context/user_preferences/actions";
 import { Outlet } from "react-router-dom";
 import ErrorBoundary from "../../components/ErrorBoundary";
 
 
 const HomeLayout: React.FC = () => {
-  const isAuthenticated = !!localStorage.getItem("authToken");
-  const prefrencesDispatch = usePreferencesDispatch();
-  useEffect(() => {
-    console.log(isAuthenticated)
-    if (isAuthenticated) {
-      fetchPreferences(prefrencesDispatch);
-    }
-  }, [prefrencesDispatch]);
 
   return (
     <div className="h-screen flex flex-col">
