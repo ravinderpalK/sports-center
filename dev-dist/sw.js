115263f909ca5520c55156e3424c93209867294b
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-11-08 22:05:19 +0530

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sg7ulao9rl"
+    "revision": "0.svapp7pnil"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
