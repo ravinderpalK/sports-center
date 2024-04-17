9208ead25e774f54b8fabbc771f821766a398131
ravinderpalK <ravinderpalsinghkhokhar@gmail.com>
2024-04-17 15:34:50 +0530

    added language change option

--- src/layouts/home/Appbar.tsx
+++ src/layouts/home/Appbar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { Link, useLocation } from "react-router-dom";
 import Prefrences from "../../views/preferences";
+import { useTranslation } from "react-i18next";
 
 const Appbar: React.FC = () => {
   const isAuthenticated = !!localStorage.getItem("authToken");
@@ -13,6 +14,14 @@ const Appbar: React.FC = () => {
       { name: 'signin', href: 'signin', current: false },
       { name: 'signup', href: 'signup', current: false },
     ];
+  
+  const preferedLanguages = [
+    { name: "English", value:"en"}, 
+    { name: "Hindi", value: "hi"}
+  ]
+  const {i18n: {changeLanguage, language} } = useTranslation();
+
+  const {t} = useTranslation();
 
   const location = useLocation();
 
@@ -22,18 +31,62 @@ const Appbar: React.FC = () => {
     </svg>;
 
   const classNames = (...classes: string[]): string => classes.filter(Boolean).join(' ');
+  // const {t} = useTranslation();
+
+  const [currentLanguage, setCurrentLanguage] = useState(language); 
+  console.log(currentLanguage)
+  const handleChangeLanguage = (newLanguage : string) => {
+    setCurrentLanguage(newLanguage);
+    changeLanguage(newLanguage);
+  }
 
   return (
     <div className="bg-gray-100">
       <div className=" text-gray-800 relative mx-auto w-16/17 p-2 flex justify-center">
         <div>
-          <Link to={'/'} className="font-bold text-xl lg:text-2xl">Sports Center</Link>
+          <Link to={'/'} className="font-bold text-xl lg:text-2xl">{t('projectTitle')}</Link>
         </div>
         <div className="flex absolute right-0">
           <div>
             {isAuthenticated &&
               (<Prefrences button={preferencesButton} />)}
           </div>
+          <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button aria-label="profile" className="rounded-full text-gray-700 hover:text-gray-900">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                      </svg>
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        {preferedLanguages.map((item) => (
+                          <Menu.Item key={item.name}>
+                            {({ active }) => (
+                              <div
+                                onClick={() => handleChangeLanguage(item.value)}
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                {item.name}
+                              </div>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
           {location.pathname == '/' ? (
             <Menu as="div" className="relative ml-3 z-20">
               <Menu.Button id="account" aria-label="AccountSettings">
